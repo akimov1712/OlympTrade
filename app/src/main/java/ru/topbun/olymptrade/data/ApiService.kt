aad15234d2ebf1d8788219ff0e1a38cb504838a6
topbun <akimov1712@mail.ru>
2024-04-10 16:50:21 +0300

    create slide bars

--- app/src/main/java/ru/topbun/olymptrade/data/ApiService.kt
+++ app/src/main/java/ru/topbun/olymptrade/data/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 interface ApiService {
 
 
-    @GET("aggs/ticker/AAPL/range/30/minute/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=paIA2qRaKvMoIV4J_oJJ7pw__y6s7juX")
+    @GET("aggs/ticker/AAPL/range/30/minute/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=paIA2qRaKvMoIV4J_oJJ7pw__y6s7juX")
     suspend fun loadBars(): Response
 
 }
--- app/src/main/java/ru/topbun/olymptrade/presentation/Terminal.kt
+++ app/src/main/java/ru/topbun/olymptrade/presentation/Terminal.kt
@@ -2,8 +2,14 @@ package ru.topbun.olymptrade.presentation
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -11,34 +17,48 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import ru.topbun.olymptrade.data.Bar
+import kotlin.math.roundToInt
+
+private const val MIN_VISIBLE_BARS = 20
+private const val MAX_VISIBLE_BARS = 1000
 
 @Composable
 fun Terminal(bars: List<Bar>) {
+    var visibleBarsCount by rememberSaveable {
+        mutableStateOf(100)
+    }
+    var takeBarsFrom by rememberSaveable {
+        mutableStateOf(0)
+    }
+    var displacement by rememberSaveable {
+        mutableStateOf(0f)
+    }
+    val transformableState = TransformableState { zoomChange, panChange, _ ->
+        visibleBarsCount = (visibleBarsCount / zoomChange).roundToInt()
+            .coerceIn(MIN_VISIBLE_BARS, MAX_VISIBLE_BARS)
+        displacement = panChange.getDistance()
+    }
     Canvas(modifier = Modifier
         .fillMaxSize()
         .background(Color.Black)
+        .transformable(transformableState)
     ) {
-        val path = Path()
-        val widthBar = size.width / bars.size
+        val visibleBars = bars.slice(takeBarsFrom..takeBarsFrom+visibleBarsCount)
+        val widthBar = size.width / visibleBars.size
         val max = bars.maxBy { it.high }.high
         val min = bars.minBy { it.low }.low
         val pixelOnePoint = size.height / (max - min)
-        bars.forEachIndexed { index, bar ->
-            val offsetX = widthBar * index
+        takeBarsFrom += (displacement / widthBar).roundToInt()
+        visibleBars.forEachIndexed { index, bar ->
+            val offsetX = size.width - widthBar * index
             val startY = size.height - (bar.low - min) * pixelOnePoint
             val endY = size.height - (bar.high - min) * pixelOnePoint
-            if (index == 0){
-                path.moveTo(offsetX, startY)
-            } else {
-                path.lineTo(offsetX,startY)
-            }
-//            drawLine(
-//                color = Color.White,
-//                start = Offset(offsetX,startY),
-//                end = Offset(offsetX,endY),
-//                strokeWidth = 1.5f
-//            )
+            drawLine(
+                color = Color.White,
+                start = Offset(offsetX,startY),
+                end = Offset(offsetX,endY),
+                strokeWidth = widthBar - 2
+            )
         }
-        drawPath(path = path, Color.White, style = Stroke(1.dp.toPx()))
     }
 }
